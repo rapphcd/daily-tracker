203d847bc576adcaa4716e159798d99798408531
rapphcd <77208630+rapphcd@users.noreply.github.com>
2026-06-16 21:12:04 +0200

    edit(modal): modification du modal des stats.

--- src/views/modal/ModalStats.tsx
+++ src/views/modal/ModalStats.tsx
@@ -49,8 +49,8 @@ function ModalStats() {
 					prov[k] = regist;
 				}
 			}
-			setAvgSleep(Math.round(100*(sleep.hr/sleep.nb))/100)
-			setAvgWork(Math.round(100*(work.hr/work.nb))/100);
+			setAvgSleep(Math.round(1000*(sleep.hr/sleep.nb))/1000)
+			setAvgWork(Math.round(1000*(work.hr/work.nb))/1000);
 			return prov;
 		}
 		const getFormattedStats = () => {
@@ -75,82 +75,135 @@ function ModalStats() {
 		<div>
 			{
 				selectedLog != undefined && (
-					<div>
+					<div style={{
+						height: "100%",
+						width: '100%',
+						display: "flex",
+						flexDirection: "column",
+						gap: "10px"
+					}}>
 						<h2 style={{
 							textAlign: "center",
-							padding: "0 1rem 1rem 1rem",
+							padding: "0 1rem 0.5rem 1rem",
 							margin: "0"
 						}}>Statistics</h2>
 						<div style={{
-							display: "grid",
-							gridTemplateColumns: "repeat(2, 1fr)",
-							gap: "50px"
-						}}>
-							<BarChart
-								style={{ width: '100%', maxWidth: '900px', maxHeight: '90vh', minHeight: "20vh", aspectRatio: 1.618 }}
-								responsive
-								data={Object.keys(logs).sort((a,b) => new Date(a) > new Date(b) ? 1 : -1).slice(-7).map((k) => {
-									const log = logs[k];
-									if(log != undefined){
-										return {name: log.date, sleeptime: log.sleepTime, worktime: log.workTime}
-									}
-									return undefined
-								})}
-								margin={{
-									top: 5,
-									right: 0,
-									left: 0,
-									bottom: 5,
-								}}
-							>
-								<CartesianGrid strokeDasharray="3 3" />
-								<XAxis dataKey="name" />
-								<YAxis width="auto" />
-								<Tooltip />
-								<Legend />
-								<Bar dataKey="sleeptime" fill="#8884d8" activeBar={{ fill: '#8884d8', stroke: 'black' }} onClick={(b) => {
-									if(b.name != undefined) setActiveLog(b.name, false);
-								}} radius={[5, 5, 0, 0]} />
-								<Bar dataKey="worktime" fill="#82ca9d" activeBar={{ fill: '#82ca9d', stroke: 'black' }} onClick={(b) => {
-									if(b.name != undefined) setActiveLog(b.name, false);
-								}} radius={[5, 5, 0, 0]} />
-								<RechartsDevtools />
-							</BarChart>
-							{
-								data.length >= 3 ? (
-									<RadarChart
-										style={{ width: '100%', height: '100%', maxWidth: '550px', maxHeight: '80vh', aspectRatio: 1 }}
-										responsive
-										outerRadius="80%"
-										data={data}
-										margin={{
-											top: 20,
-											left: 20,
-											right: 20,
-											bottom: 20,
-										}}
-									>
-										<PolarGrid />
-										<PolarAngleAxis dataKey="habit" />
-										<PolarRadiusAxis />
-										<Radar name="Mike" dataKey="B" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-										<RechartsDevtools />
-									</RadarChart>
-								) : (
-									<div style={{ width: '100%', height: '100%', display: "flex", justifyContent: "center", alignItems: "center" }}>
-										<h3>Habits graph unavailable.</h3>
-									</div>
-								)
-							}
-							<div>
+							display: "flex",
+							flexWrap: "nowrap",
+							gap: "10px"
+						}} >
+							<div style={{
+								backgroundColor: "var(--background-secondary)",
+								borderRadius: "var(--radius-l)",
+								padding: "1rem",
+								width: '100%',
+								flex: "1"
+							}}>
+								<BarChart
+									style={{ width: '100%', height: "100%", maxHeight: '90vh', aspectRatio: 1.618 }}
+									responsive
+									data={Object.keys(logs).sort((a,b) => new Date(a) > new Date(b) ? 1 : -1).slice(-7).map((k) => {
+										const log = logs[k];
+										if(log != undefined){
+											return {name: log.date, sleeptime: log.sleepTime, worktime: log.workTime}
+										}
+										return undefined
+									})}
+									margin={{
+										top: 5,
+										right: 0,
+										left: 0,
+										bottom: 5,
+									}}
+								>
+									<CartesianGrid strokeDasharray="3 3" />
+									<XAxis dataKey="name" />
+									<YAxis width="auto" />
+									<Tooltip />
+									<Legend />
+									<Bar dataKey="sleeptime" fill="#8884d8" activeBar={{ fill: '#8884d8', stroke: 'black' }} onClick={(b) => {
+										if(b.name != undefined) setActiveLog(b.name, false);
+									}} radius={[5, 5, 0, 0]} />
+									<Bar dataKey="worktime" fill="#82ca9d" activeBar={{ fill: '#82ca9d', stroke: 'black' }} onClick={(b) => {
+										if(b.name != undefined) setActiveLog(b.name, false);
+									}} radius={[5, 5, 0, 0]} />
+									<RechartsDevtools />
+								</BarChart>
+							</div>
+							<div style={{
+								backgroundColor: "var(--background-secondary)",
+								borderRadius: "var(--radius-l)",
+								padding: "0.2rem",
+								width: '100%',
+								flex: "1"
+							}}>
+								{
+									data.length >= 3 ? (
+										<RadarChart
+											style={{ width: '100%', height: "100%", aspectRatio: 1.618 }}
+											responsive
+											outerRadius="80%"
+											data={data}
+											margin={{
+												top: 20,
+												left: 20,
+												right: 20,
+												bottom: 20,
+											}}
+										>
+											<PolarGrid />
+											<PolarAngleAxis dataKey="habit" />
+											<PolarRadiusAxis />
+											<Radar name="Mike" dataKey="B" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+											<RechartsDevtools />
+										</RadarChart>
+									) : (
+										<div style={{ width: '100%', height: '100%', display: "flex", justifyContent: "center", alignItems: "center" }}>
+											<h3>Habits graph unavailable.</h3>
+										</div>
+									)
+								}
+							</div>
+						</div>
+						<div style={{
+							display: "flex",
+							flexWrap: "nowrap",
+							gap: "10px"
+						}} >
+
+							<div  style={{
+								backgroundColor: "var(--background-secondary)",
+								borderRadius: "var(--radius-l)",
+								height: "fit-content",
+								padding: "1rem",
+								width: "100%"
+							}}>
 								<h3  style={{
 									textAlign: "center",
-									padding: "0 1rem 1rem 1rem",
+									padding: "0 1rem 0 1rem",
 									margin: "0"
 								}}>Averages</h3>
-								<div>
-									<p>{avgSleep}hr</p>
-									<p>{avgWork}hr</p>
+								<div style={{
+									display: "flex"
+								}}>
+									<div>
+										<h4 style={{
+											margin: "0.5rem 0 0.5rem 0"
+										}}>Sleep</h4>
+										<p style={{
+											fontSize: "10px"
+										}}>Daily Average</p>
+										<p>{avgSleep}hr</p>
+									</div>
+									<div>
+										<h4 style={{
+											margin: "0.5rem 0 0.5rem 0"
+										}}>Work</h4>
+										<p style={{
+											fontSize: "10px"
+										}}>Daily Average</p>
+										<p>{avgWork}hr</p>
+									</div>
 								</div>
 							</div>
 						</div>
